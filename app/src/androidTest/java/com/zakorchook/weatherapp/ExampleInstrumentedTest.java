9d74268b93a3b82b30498ba7b181624af51a9e6c
Zaharchuk Mihail <zakorchook@gmail.com>
2016-10-12 16:44:28 +0300

    - add simple espresso instrumental test

---
+++ app/src/androidTest/java/com/zakorchook/weatherapp/ExampleInstrumentedTest.java
@@ -0,0 +1,61 @@
+package com.zakorchook.weatherapp;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDescendantOfA;
+import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withSpinnerText;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.allOf;
+import static org.hamcrest.CoreMatchers.anything;
+import static org.junit.Assert.*;
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+
+    private static final String[] keyNamesList = new String[]{"Киев", "Харьков", "Запорожье"};
+
+//    @Test
+//    public void useAppContext() throws Exception {
+//        // Context of the app under test.
+//        Context appContext = InstrumentationRegistry.getTargetContext();
+//
+//        assertEquals("com.zakorchook.weatherapp", appContext.getPackageName());
+//    }
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void testUi() throws Exception {
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext());
+        for (String s : keyNamesList) {
+            prefsItemClick(s);
+        }
+    }
+
+    private void prefsItemClick(final String itemName){
+        onView(withId(R.id.action_settings)).perform(click());
+        onView(withText(itemName)).perform(click());
+    }
+}
